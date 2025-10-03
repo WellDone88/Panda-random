8c585d56b3c608cae589225b691479a068b7fbbd
WellDone88 <kenneth.moren@live.no>
2025-10-03 10:24:06 +0200

    Update view.js

--- view.js
+++ view.js
@@ -1,12 +1,12 @@
 function updateView(){
 let HTML = document.getElementById("app").innerHTML = /*HTML*/ `
 <div><img id="M8" src="Magic_8_ball.png"></div>
-<h2>Still et spørsmål høyt og trykk på spå knappen</h2>
+<h2>Hvem er den heldige?</h2>
 
 <!-- Bakgrunnsmusikk som fungerer ved play -->
 <audio src="Magic Escape Room.mp3" controls></audio>
 
-<button onclick="TellFortune()">Spå meg</button>
+<button onclick="TellFortune()">Lets rumble</button>
 <p id="Answers"><p>
 
 
