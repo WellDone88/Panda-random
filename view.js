85e2a1e4cf6752be4d733ff7f459138351341f05
WellDone88 <kenneth.moren@live.no>
2025-10-03 11:38:39 +0200

    sda

--- view.js
+++ view.js
@@ -1,7 +1,9 @@
 function updateView(){
 let HTML = document.getElementById("app").innerHTML = /*HTML*/ `
-<div><img id="M8" src="Magic_8_ball.png"></div>
+<div><img id="M8" src="Magic_8_ball.png"></div> 
+<img src="pandaLogo.png">
 <h2>Hvem er den heldige?</h2>
+
 
 <!-- Bakgrunnsmusikk som fungerer ved play -->
 <audio src="Magic Escape Room.mp3" controls></audio>
